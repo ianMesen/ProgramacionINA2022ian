9db2ee44442a03790d698e9d78d0a5df77f91a12
ian mesen <ianmesencruz@gmail.com>
2023-03-15 07:45:29 +0100

    apiRick

--- paginas web 4/Asincronico/ian/rick/rickaandmorty/apiRick/src/app/pages/personajes/detalle-personaje/detalle-personaje.component.ts
+++ paginas web 4/Asincronico/ian/rick/rickaandmorty/apiRick/src/app/pages/personajes/detalle-personaje/detalle-personaje.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, SchemaMetadata } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Personaje } from 'src/app/shared/models/personaje';
 import { PersonajesService } from 'src/app/shared/services/personajes.service';
@@ -10,17 +11,29 @@ import { PersonajesService } from 'src/app/shared/services/personajes.service';
 })
 export class DetallePersonajeComponent {
   personaje: Personaje;
- 
+  // linkEpisodio:string;
+  // id:string;
+  // episodios:string[]=['id','name','air_date','episode','created']
   constructor(route:ActivatedRoute ,srv:PersonajesService){
 
     const id= route.snapshot.paramMap.get('id');
     
-    srv.getPersonajeById(id).subscribe((per:any)=>{
-      this.personaje=per;
-      console.log(this.personaje);
+    srv.getPersonajeById(id).subscribe((result:any)=>{
+
+      const {episode}=result;
+      this.personaje=result;
+      this.personaje.episode=episode;
+      console.log(this.personaje.episode);
     })
   }
-
+  getIdEpisodio(episode:string){
+    let linkEpisodio = episode.split("sode/")
+    let id =linkEpisodio[1];
+    // console.log(id);
+    return id;
+  
+    
+  }
 
 
 }
--- paginas web 4/Asincronico/ian/rick/rickaandmorty/apiRick/src/app/shared/models/personaje.ts
+++ paginas web 4/Asincronico/ian/rick/rickaandmorty/apiRick/src/app/shared/models/personaje.ts
@@ -9,6 +9,6 @@ export interface Personaje{
     gender:String;
     image: String;
     created:Date;
-
+    episode:[];
 }
 
